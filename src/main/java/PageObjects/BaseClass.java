fe52d416c1a4e838250179bff983a86291128e92
Fizza <fizzajaw12@gmail.com>
2023-06-06 22:20:04 +0500

    Updated pom.xml and webdriver

--- src/main/java/PageObjects/BaseClass.java
+++ src/main/java/PageObjects/BaseClass.java
@@ -1,7 +1,9 @@
 package PageObjects;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class BaseClass {
 
@@ -9,10 +11,17 @@ public class BaseClass {
     public static WebDriver driver;
 
     public static void setDriver(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Spursol-Shayan\\Documents\\WebDriver\\chromedriver.exe");
 
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\Spursol-Shayan\\Documents\\WebDriver\\chromedriver.exe");
+
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://vllenderspr1.spurams.com/");
+
+
+
+
+
     }
 
 }
